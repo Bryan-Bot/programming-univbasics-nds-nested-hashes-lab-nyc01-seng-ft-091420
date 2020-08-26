19cf8a177d93e1f0a5e33a5dfec46b1fd2b26c98
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-26 14:53:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,10 +16,6 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   array = []
-  array[0] = DON_G
-  array[1] = JOELLE_VD
-  array[2] = PAT_M
-  array[3] = KATE_G
   array[4] = BRUCE_G
 end
 
